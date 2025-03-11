70c9f68b876a899ce7480259998d1ad545723c24
M <CohortMinseok@users.noreply.github.com>
2025-03-10 21:17:27 -0500

    Create RetSpoofInvoker.h

---
+++ RetSpoofInvoker.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <cstdint>
+
+#include "x64RetSpoof.h"
+
+class RetSpoofInvoker {
+private:
+	void* gadgetAddress{ 0 };
+public:
+	void init(std::uintptr_t gadgetAddress) noexcept
+	{
+		this->gadgetAddress = reinterpret_cast<void*>(gadgetAddress);
+	}
+
+	template <typename ReturnType, typename... Args>
+	ReturnType invokeFastcall(std::uintptr_t functionAddress, Args&&... args) const noexcept
+	{
+		return x64RetSpoof::FunctionTraits<ReturnType(Args...)>::spoof_call(this->gadgetAddress, reinterpret_cast<void*>(functionAddress), std::forward<Args>(args)...);
+	}
+};
+
+inline RetSpoofInvoker invoker;
